77f4b44291fdc9ceaad6be7920cd609aa49f4e42
baseballclub <abkimdoyo25@gmail.com>
2025-05-01 23:29:29 +0900

    [Feat] 덧셈, 곱셈 기능

---
+++ src/main/java/com/likelion/session/controller/calculator/CalculatorController.java
@@ -0,0 +1,32 @@
+package com.likelion.session.controller.calculator;
+
+import com.likelion.session.dto.calculator.request.CalculatorAddRequest;
+import com.likelion.session.dto.calculator.request.CalculatorDivRequest;
+import com.likelion.session.dto.calculator.request.CalculatorMultiplyRequest;
+import com.likelion.session.dto.calculator.request.CalculatorSubRequest;
+import com.likelion.session.service.calculator.CalculatorService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/calculator")
+public class CalculatorController {
+
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping("/add")
+    public int addTwoNumbers(CalculatorAddRequest request) {
+        return calculatorService.add(request.getNumber1(), request.getNumber2());
+    }
+
+    @PostMapping("/multiply")
+    public int multiplyTwoNumbers(@RequestBody CalculatorMultiplyRequest request) {
+        return calculatorService.multiply(request.getNumber1(), request.getNumber2());
+    }
+
+
+
+}
---
+++ src/main/java/com/likelion/session/service/calculator/CalculatorService.java
@@ -0,0 +1,14 @@
+package com.likelion.session.service.calculator;
+
+public class CalculatorService {
+
+    public int add(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    public int multiply(int number1, int number2) {
+        return number1 * number2;
+    }
+
+
+}
